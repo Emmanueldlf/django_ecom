226b817b36ef0e31e4334fb26db9ea12d226c78f
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-08-03 23:43:31 +0900

    added a customized action to product, change category to default

--- shop/admin.py
+++ shop/admin.py
@@ -9,8 +9,15 @@ admin.site.site_title = "Net Shop"
 admin.site.index_title = "Manage Net Shop"
 
 class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category = "default")
+
+
+    change_category_to_default.short_description = 'Default Category'
     list_display = ('name','price','discounted_price','category','description','image')
     search_fields = ('name',)
+    actions = ('change_category_to_default',)
 
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Order)
