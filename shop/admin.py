5cfa9001d473b24008233dd230b51b9a56fb1b11
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-08-03 23:22:15 +0900

    customised site admin panel with a customized title and header

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,10 @@ from .models import Product
 from .models import Order
 
 # Register your models here.
+
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "Net Shop"
+admin.site.index_title = "Manage Net Shop"
+
 admin.site.register(Product)
 admin.site.register(Order)
